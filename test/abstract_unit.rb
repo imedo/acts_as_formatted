654faf7e65844a9d6b8470ae3130daaf92e6a242
Thomas Kadauke <tkadauke@imedo.de>
2009-05-14 16:10:05 +0200

    use fixed rails version

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -3,4 +3,4 @@ require 'rubygems'
 require 'dry_plugin_test_helper'
 require 'mocha'
 
-PluginTestEnvironment.initialize_environment(File.dirname(__FILE__))
+PluginTestEnvironment.initialize_environment(File.dirname(__FILE__), :rails_version => '2.0.2')
